2adefa67bcdc588346c5c4cc3e9e4b50ad8a37de
Zachary Kain <zakkain@gmail.com>
2012-07-26 16:30:09 -0400

    figuring out way to keep boom synced

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,9 @@ alias git='nocorrect git'
 alias boom='nocorrect boom'
 
 alias sublime='subl'
+alias edit='subl'
+
+# this starts up a super-light localhost
+# usage:
+# $ server
+alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
