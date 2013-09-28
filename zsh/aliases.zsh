f6bacff092a85d3dff3773fe85450fc36784dd9d
Zachary Kain <zakkain@gmail.com>
2013-09-27 21:03:00 -0400

    cleaning house

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -37,12 +37,10 @@ alias habitrpg="BASE_URL="http://localhost:3000" FACEBOOK_KEY=abc FACEBOOK_SECRE
 
 
 # this starts up a super-light localhost
-# usage:
-# $ server
 alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
 
 # linking and symlinking
-alias symlink='ln -s'
+alias symlink='ln -s' # ln -s PLACE NEWPLACE
 alias hardlink='ln'
 
 # basic terminal operations
@@ -54,7 +52,9 @@ alias delete='rm -r'
 alias copy='cp -r'
 
 # SSH
+alias sshconfig='~/.ssh/config'
 alias moonbase='ssh zacharyk@zacharykain.com -p 7822'
+alias fixssh='ssh-add ~/.ssh/moonbase'
 
 #qq by brett terpstra
 alias qq='~/.dotfiles/osx/qq'
@@ -69,3 +69,11 @@ alias postsqlstop='pg_ctl -D /usr/local/var/postgres -l logfile stop'
 
 # sync gif folder to amazon bucket
 alias loadgifs='s3cmd sync --delete-removed ~/Dropbox/Public/_macros/ s3://zkkn.co/'
+
+alias beExecutable='chmod +x'
+
+alias showHidden='defaults write com.apple.Finder AppleShowAllFiles Yes && killall Finder'
+alias hideHidden='defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder'
+
+alias dontsave='defaults write NSGlobalDomain NSSavePanelStandardDesktopShortcutOnly -bool YES'
+alias holdkeysoff='defaults write -g ApplePressAndHoldEnabled -bool false'
